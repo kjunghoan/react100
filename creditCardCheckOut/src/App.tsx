e55bd25561a225518c16174a78b57dc3cbca570d
Jung Hoan Kim <kjunghoan@gmail.com>
2023-10-07 17:24:28 -0400

    init

---
+++ creditCardCheckOut/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Credit Card Form</h2>
+      <form>
+        <div>
+          <label>Credit Card Number</label>
+          <input />
+        </div>
+        <div>
+          <label>CVV Number</label>
+          <input />
+        </div>
+        <div>
+          <label>Credit Card Number</label>
+          <input />
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default App
